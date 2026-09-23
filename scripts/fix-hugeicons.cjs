3031a4cb32bb8e0c836b43a7f9d4edcc37e6085c
JSMVALOR <JSMVALOR@users.noreply.github.com>
2026-09-23 12:14:38 +0530

    Fix: Add Grid3x3 to the hugeicons patch regex

--- scripts/fix-hugeicons.cjs
+++ scripts/fix-hugeicons.cjs
@@ -7,15 +7,13 @@ const indexCjsFile = path.join(__dirname, '../node_modules/@hugeicons/core-free-
 try {
   if (fs.existsSync(indexFile)) {
     let content = fs.readFileSync(indexFile, 'utf8');
-    content = content.replace(/from '\.\/Grid2x2/g, "from './Grid2X2");
-    content = content.replace(/from '\.\/Grid3x2/g, "from './Grid3X2");
+    content = content.replace(/from '\.\/Grid(\d)x(\d)/g, "from './Grid$1X$2");
     fs.writeFileSync(indexFile, content);
     console.log("Fixed ESM hugeicons imports");
   }
   if (fs.existsSync(indexCjsFile)) {
     let content = fs.readFileSync(indexCjsFile, 'utf8');
-    content = content.replace(/require\('\.\/Grid2x2/g, "require('./Grid2X2");
-    content = content.replace(/require\('\.\/Grid3x2/g, "require('./Grid3X2");
+    content = content.replace(/require\('\.\/Grid(\d)x(\d)/g, "require('./Grid$1X$2");
     fs.writeFileSync(indexCjsFile, content);
     console.log("Fixed CJS hugeicons imports");
   }
